ae686d1b07925753775dbf82803cb9884f85afc4
Ruslan <89846874+True-Ruslan@users.noreply.github.com>
2026-08-09 12:44:08 +0300

    feat: add minimal Spring Boot application

---
+++ apps/api/src/main/java/io/github/trueruslan/zakupgotov/ZakupGotovApplication.java
@@ -0,0 +1,12 @@
+package io.github.trueruslan.zakupgotov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZakupGotovApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZakupGotovApplication.class, args);
+    }
+}
