708571cd906d82129f19e8f8540de6bea219b836
Ruslan <89846874+True-Ruslan@users.noreply.github.com>
2026-08-09 13:01:27 +0300

    test: use shared PostgreSQL integration support

--- apps/api/src/test/java/io/github/trueruslan/zakupgotov/database/PostgresIntegrationTest.java
+++ apps/api/src/test/java/io/github/trueruslan/zakupgotov/database/PostgresIntegrationTest.java
@@ -5,30 +5,11 @@ import org.jooq.Record;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.postgresql.PostgreSQLContainer;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@Testcontainers
 @SpringBootTest
-class PostgresIntegrationTest {
-
-    @Container
-    static final PostgreSQLContainer POSTGRES = new PostgreSQLContainer("postgres:18.4")
-            .withDatabaseName("zakup_gotov")
-            .withUsername("zakup_gotov")
-            .withPassword("test-only-password");
-
-    @DynamicPropertySource
-    static void configureDatasource(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", POSTGRES::getJdbcUrl);
-        registry.add("spring.datasource.username", POSTGRES::getUsername);
-        registry.add("spring.datasource.password", POSTGRES::getPassword);
-    }
+class PostgresIntegrationTest extends PostgresIntegrationSupport {
 
     @Autowired
     DSLContext dsl;
