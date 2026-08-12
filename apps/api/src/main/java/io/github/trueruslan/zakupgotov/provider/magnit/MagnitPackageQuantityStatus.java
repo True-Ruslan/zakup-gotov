0b0f3f1162f650780398f9e9a8bd69e9bfb56dc8
Ruslan <89846874+True-Ruslan@users.noreply.github.com>
2026-08-12 19:55:35 +0300

    feat(magnit): add package extraction status

---
+++ apps/api/src/main/java/io/github/trueruslan/zakupgotov/provider/magnit/MagnitPackageQuantityStatus.java
@@ -0,0 +1,9 @@
+package io.github.trueruslan.zakupgotov.provider.magnit;
+
+public enum MagnitPackageQuantityStatus {
+    FOUND,
+    MISSING,
+    AMBIGUOUS_DIMENSIONS,
+    CONFLICTING_VALUES,
+    INVALID_VALUE
+}
